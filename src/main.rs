be79283622f8ac62063ddb8e4583eddb325fd091
Mortrix <sebastiansommerdk@gmail.com>
2026-06-26 15:20:03 +0200

    Added build dependencies for wayland

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+use macroquad::miniquad;
+
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "Crispy Pancake".to_owned(),
+        platform: miniquad::conf::Platform {
+            linux_backend: miniquad::conf::LinuxBackend::WaylandWithX11Fallback,
+            ..Default::default()
+        },
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_conf)]
+async fn main() {
+    loop {
+        clear_background(BLUE);
+        draw_text("Hello, Macroquad!", 50.0, 50.0, 50.0, BLACK);
+
+        next_frame().await
+    }
 }
