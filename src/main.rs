24fe00e693d74e138cf84b92bd0ea83e99d2596c
magnuslsc <magnuslsc@gmail.com>
2026-06-26 16:58:44 +0200

    Tested in main

--- src/main.rs
+++ src/main.rs
@@ -21,15 +21,58 @@ fn window_conf() -> Conf {
     }
 }
 
+pub fn handle_key_event_test(x_pos: &mut f32, y_pos: &mut f32) {
+    if is_key_down(KeyCode::Left) {
+        *x_pos -= 1.0;
+    }
+    if is_key_down(KeyCode::Right) {
+        *x_pos += 1.0;
+    }
+    if is_key_down(KeyCode::Up) {
+        *y_pos -= 1.0;
+    }
+    if is_key_down(KeyCode::Down) {
+        *y_pos += 1.0;
+    } 
+}
+
 #[macroquad::main(window_conf)]
 async fn main() {
     let mut state: GameState;
     
+    let (mut x_pos, mut y_pos) = (100.0, 100.0);
+    
+    let temp_player = load_texture("assets/Free/Main_Characters/Pink Man/Jump (32x32).png")
+        .await
+        .expect("Error loading player texture.");
+
+    let background = load_texture("assets/Free/Background/Blue.png")
+        .await
+        .expect("Error loading background texture");
+
     loop {
         // Delta time
         let dt = get_frame_time();
         clear_background(BLUE);
         draw_text("Hello, Macroquad!", 50.0, 50.0, 50.0, BLACK);
+        handle_key_event_test(&mut x_pos, &mut y_pos);
+
+        // Draw background
+        draw_texture_ex(
+            &background,
+            0.0,
+            0.0,
+            WHITE,
+            DrawTextureParams {
+                dest_size: Some(vec2(
+                    screen_width(),
+                    screen_height(),
+                )),
+                ..Default::default()
+            },
+        );
+        // Draw player
+        draw_texture(&temp_player, x_pos, y_pos, RED);
 
         next_frame().await
     }
